894fede6a92f717b1594a4ae2d560e147197768f
TusharJain213 <134579001+TusharJain213@users.noreply.github.com>
2023-10-02 10:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (30.09%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool f(int a,vector<int>&nums,int k){
+        int sum=0;
+        int cnt=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]+sum<=a){
+                  sum+=nums[i];
+            }
+            else{
+                cnt++;
+                sum=nums[i];
+            }
+        }
+        if(cnt>k){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int splitArray(vector<int>& nums, int k) {
+       int start=*max_element(nums.begin(),nums.end());
+       int end=accumulate(nums.begin(),nums.end(),0);
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(f(mid,nums,k)){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+     return start;
+    }
+};
