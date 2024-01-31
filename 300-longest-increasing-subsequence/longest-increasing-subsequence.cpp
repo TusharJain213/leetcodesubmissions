82e16b0b1f0e73b0c8cf55d3066b51e65319e2c1
TusharJain213 <134579001+TusharJain213@users.noreply.github.com>
2024-02-01 01:34:12 +0530

    Time: 60 ms (65.39%) | Memory: 12.8 MB (26.67%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,9 +14,18 @@ public:
        }
        return dp[idx][last+1]=max(x,y);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(0,-1,nums,dp);
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp(n,1);
+	 int maxi=1;
+	 for(int i=0;i<n;i++){
+         for(int j=0;j<i;j++){
+			 if(arr[i]>arr[j]){
+				 dp[i]=max(1+dp[j],dp[i]);
+				 maxi=max(maxi,dp[i]);
+			 }
+		 }
+	 }
+	 return maxi;
     }
 };
